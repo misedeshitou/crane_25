8f6123ad1c8f4dc868c7df7b8881aba1333df515
misedeshitou <3299751760@qq.com>
2025-06-09 10:33:49 +0800

    init

---
+++ applications/behaviours/behaviour_status.hpp
@@ -0,0 +1,10 @@
+#ifndef BEHAVIOR_STATUS_HPP
+#define BEHAVIOR_STATUS_HPP
+
+enum class BehaviorStatus
+{
+  SUCCESS,
+  RUNNING,
+};
+
+#endif  // BEHAVIOR_STATUS_HPP
---
+++ applications/behaviours/behaviours_tick.hpp
@@ -0,0 +1,7 @@
+#ifndef BEHAVIOR_TICK_HPP
+#define BEHAVIOR_TICK_HPP
+
+constexpr int BEHAVIOR_TICK_MS = 1;           // ms
+constexpr float T = BEHAVIOR_TICK_MS * 1e-3;  // s
+
+#endif  // BEHAVIOR_TICK_HPP
